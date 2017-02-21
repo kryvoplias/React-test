4ac37349eed8135cc5fb8efe2c3f05fdbb1e8b9e
Maksym Kryvoplias <m.kryvoplias@gmail.com>
2017-02-21 17:06:28 +0200

    Adding call to initialize action on startup.

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 'use strict';
 
 var React = require('react');
-var Router = require('react-router');
 var routes = require('./routes');
+var Router = require('react-router');
+var InitializeActions = require('./actions/initializeActions');
+
+InitializeActions.initApp();
 
 Router.run(routes, /*Router.HistoryLocation,*/ function (Handler) {
     React.render(<Handler />, document.getElementById('app'));
